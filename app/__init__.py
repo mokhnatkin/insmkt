89d960a91c34e47baee15c57153c22a992b4c795
Мохнаткин Алексей <a.mokhnatkin@a-i.kz>
2019-06-06 14:52:09 +0600

    some packages removed

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
@@ -16,10 +14,8 @@ import flask_excel as excel
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
-mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
